44a00baa57dbd69f796b921a0f94d254abfc1bb4
ncos8447 <naimacosper22@gmail.com>
2026-02-28 02:39:10 +0100

    Week6Exercise1

--- main.py
+++ main.py
@@ -1,88 +1,34 @@
-import string
+#Music library manager
+#Step 1
 
-#Part A
+songs = []
+genre_count = {}
 
-def check_min_length(password, min_len=8):
-    return len(password) >= min_len
+print("Welcome to Music Library Manager!")
 
-def has_uppercase(password):
-    return any(char in string.ascii_uppercase for char in password)
+#Step 2-3
 
-def has_lowercase(password):
-    return any(char in string.ascii_lowercase for char in password)
+for i in range(1, 6):
+    print(f"\nEnter song {i}:")
+    title = str(input(" Song name: "))
+    genre = str(input(" Genre: "))
 
-def has_digit(password):
-    return any(char in string.digits for char in password)
+    info = (title, genre)
+    songs.append(info)
 
-def has_special_char(password):
-    return any(char in string.punctuation for char in password)
+    genre_count[genre] = genre_count.get(genre, 0) + 1
 
-#Part B
+#Step 4
 
-def validate_password(password):
-    results = {
-        "min_length": check_min_length(password),
-        "has_uppercase": has_uppercase(password),
-        "has_lowercase": has_lowercase(password),
-        "has_digit": has_digit(password),
-        "has_special": has_special_char(password)
-    }
-    results["is_valid"] = all(results.values())
-    return results
+print("\n=== YOUR MUSIC LIBRARY ===")
 
-def user_interface():
-    print("PASSWORD VALIDATOR\n"
-          "\nPassword requirements:\n"
-          "Minimum 8 characters\n"
-          "At least one uppercase letter\n"
-          "At least one lowercase letter\n"
-          "At least one digit\n"
-          "At least one special character (!@#$%^&* etc.)\n"
-          )
+for index, (title, genre) in enumerate(songs, 1):
+    print(f"{index}. {title} ({genre})")
 
-#Part C
+print("\n=== GENRE STATISTICS ===")
 
-    password = str(input("Enter password: "))
+for genre, count in genre_count.items:
+    print(f"{genre}: {count} songs")
 
-    valid = validate_password(password)
-
-    print("")
-    check = "✓" if valid["min_length"] else " "
-    print(f"{check} Minimum length: {"Met" if valid["min_length"] else "Not met"}\n")
-
-    check = "✓" if valid["has_uppercase"] else " "
-    print(f"{check} Contains uppercase: {"Met" if valid["has_uppercase"] else "Not met"}\n")
-
-    check = "✓" if valid["has_lowercase"] else " "
-    print(f"{check} Contains lowercase: {"Met" if valid["has_lowercase"] else "Not met"}\n")
-
-    check = "✓" if valid["has_digit"] else " "
-    print(f"{check} Contains digit: {"Met" if valid["has_digit"] else "Not met"}\n")
-
-    check = "✓" if valid["has_special"] else " "
-    print(f"{check} Contains special character: {"Met" if valid["has_special"] else "Not met"}\n")
-
-    if valid["is_valid"]:
-        print("Password is strong\n")
-    else:
-        print("Password is weak\n")
-
-    if not valid["min_length"]:
-        print(f"Password is too short")
-
-    if not valid["has_uppercase"]:
-        print(f"Add an uppercase letter")
-
-    if not valid["has_lowercase"]:
-        print(f"Add a lowercase letter")
-
-    if not valid["has_digit"]:
-        print(f"Add a number")
-
-    if not valid["has_special"]:
-        print(f"Add a special character")
-
-
-print()
-
-user_interface()
+most_popular = max(genre_count, key=genre_count.get)
+print(f"\nMost popular genre: {most_popular}")
