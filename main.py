6af86cc7dc6d562c85ac48529d9da5675aea3cb2
ncos8447 <naimacosper22@gmail.com>
2026-02-28 02:43:42 +0100

    Week6Exercise2

--- main.py
+++ main.py
@@ -1,34 +1,58 @@
-#Music library manager
-#Step 1
+#student grade analyzer
 
-songs = []
-genre_count = {}
+#part a
+student_records = []
+stats = {}
 
-print("Welcome to Music Library Manager!")
+for i in range(1, 7):
+    name = str(input(f"Student's name: "))
 
-#Step 2-3
+    #score validation
+    while True:
+        try:
+            score = float(input("  Score (0-100): "))
+            if 0 <= score <= 100:
+                break
+            else:
+                print("    Score must be between 0 and 100.")
+        except ValueError:
+            print("    Please enter a valid number.")
+    student_records.append((name, score))
 
-for i in range(1, 6):
-    print(f"\nEnter song {i}:")
-    title = str(input(" Song name: "))
-    genre = str(input(" Genre: "))
+#stats
 
-    info = (title, genre)
-    songs.append(info)
+scores = [score for name, score in student_records]
 
-    genre_count[genre] = genre_count.get(genre, 0) + 1
+stats["highest"] = max(scores)
+stats["lowest"] = min(scores)
+stats["average"] = sum(scores) / len(scores)
 
-#Step 4
+unique_scores = set(scores)
 
-print("\n=== YOUR MUSIC LIBRARY ===")
+grade_distribution = {}
+for score in scores:
+    grade_distribution[str(score)] = grade_distribution.get(str(score), 0) + 1
 
-for index, (title, genre) in enumerate(songs, 1):
-    print(f"{index}. {title} ({genre})")
+#display results
 
-print("\n=== GENRE STATISTICS ===")
+print("\n=== STUDENT RECORDS ===")
 
-for genre, count in genre_count.items:
-    print(f"{genre}: {count} songs")
+for i in range(1, 7):
+    print(f"{i}. {student_records[i-1][0]}: {student_records[i-1][1]}")
 
-most_popular = max(genre_count, key=genre_count.get)
-print(f"\nMost popular genre: {most_popular}")
+print("\n=== STATISTICS ===")
+
+print(f"Highest Score: {stats["highest"]}")
+print(f"Lowest Score: {stats["lowest"]}")
+print(f"Average Score: {stats["average"]:.2f}")
+
+print("\n=== UNIQUE SCORES ===")
+print(unique_scores)
+print(f"Total unique scores: {len(unique_scores)}")
+
+print("\n=== GRADE DISTRIBUTION ===")
+for score, number in sorted(grade_distribution.items(), reverse=True):
+    if number == 1:
+        print(f"Score {score}: {number} student")
+    else:
+        print(f"Score {score}: {number} students")
